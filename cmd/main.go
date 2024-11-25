d35f320daee24a3dd31ed0fd49dc34ce65124eb3
Philip Meholm <mephim+git@gmail.com>
2024-11-26 00:14:34 +0100

    upd main

--- cmd/main.go
+++ cmd/main.go
@@ -50,8 +50,6 @@ func init() {
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	// tools.InitSlog(Debug)
-
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
