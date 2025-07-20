d6a245d2255b6058d7a3b3afb843696cac7e61c6
Philip Meholm <mephim+git@gmail.com>
2025-07-20 18:04:16 +0200

    upd vault handlers

--- internal/vaults/registry.go
+++ internal/vaults/registry.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/huh"
+	"github.com/withholm/polyenv/internal/model"
 	"github.com/withholm/polyenv/internal/vaults/keyvault"
 )
 
 // TODO for dev!
 type VaultType string
 
-// append your valuttype
+// !DEV:append your valuttype
 const (
 	vltTypeKeyvault VaultType = "keyvault"
 )
 
-// append vault type to registry. this will be
-var Registry = map[string]Vault{
-	"keyvault": &keyvault.Client{},
+// !DEV: append vault type to registry. this will be the items i use when instanciating the vault
+// returns the registry
+func Registry() map[string]model.Vault {
+	return map[string]model.Vault{
+		"keyvault": &keyvault.Client{},
+	}
+}
+
+// returns a new instance of the vault
+func GetVaultInstance(vaultType string) (model.Vault, error) {
+	vaults := Registry()
+	vault, ok := vaults[vaultType]
+	if !ok {
+		return nil, fmt.Errorf("unknown vault type: %s", vaultType)
+	}
+	return vault, nil
 }
 
 // used by cobra. returns the type of the vault
@@ -33,7 +47,7 @@ func (vtype *VaultType) Type() string {
 // returns a list of all vault types taken from the registry
 func ListVaultTypes() []string {
 	keys := make([]string, 0)
-	for k := range Registry {
+	for k := range Registry() {
 		keys = append(keys, k)
 	}
 	return keys
@@ -41,7 +55,7 @@ func ListVaultTypes() []string {
 
 // used by cobra. sets the vault type from string
 func (vtype *VaultType) Set(value string) error {
-	_, ok := Registry[value]
+	_, ok := Registry()[value]
 	if !ok {
 		return fmt.Errorf("unknown vault type: %s. must be one of %s", value, ListVaultTypes())
 	}
@@ -51,13 +65,14 @@ func (vtype *VaultType) Set(value string) error {
 
 // used by init. lists all vault types as huh select.
 func VaultTypeSelector(ref *VaultType) *huh.Select[VaultType] {
-	opt := make([]huh.Option[VaultType], 0)
-	for k, v := range Registry {
-		opt = append(opt, huh.NewOption(v.DisplayName(), VaultType(k)))
-	}
-
 	return huh.NewSelect[VaultType]().
 		Title("Select a vault type").
-		Options(opt...).
+		OptionsFunc(func() []huh.Option[VaultType] {
+			opt := make([]huh.Option[VaultType], 0)
+			for k, v := range Registry() {
+				opt = append(opt, huh.NewOption(v.DisplayName(), VaultType(k)))
+			}
+			return opt
+		}, ref).
 		Value(ref)
 }
--- internal/vaults/vaults.go
+++ internal/vaults/vaults.go
@@ -1,142 +1,65 @@
 package vaults
 
 import (
-	"fmt"
-	"log/slog"
-	"os"
-	"strings"
-
-	"github.com/charmbracelet/huh"
-	"github.com/joho/godotenv"
-	"github.com/withholm/polyenv/internal/tools"
-
 	_ "embed"
 )
 
-type Vault interface {
-	// returns the display name of the vault
-	DisplayName() string
-	// push a single secret
-	Push(name string, value string) error
-	// pull all secrets
-	Pull() (map[string]string, error)
-	// list all secrets
-	List() ([]string, error)
-	// remove secret from remote vault
-	Flush(key []string) error
-	// tries to un-delte or un-fuck the last push/flush
-	Opsie() error
-	// Warm the vault connection.
-	Warmup() error
-	// validate the incoming config
-	ValidateConfig(options map[string]string) error
-	// set options for the vault
-	SetOptions(map[string]string) error
-	// get options for the vault
-	// GetOptions() map[string]string
+// func NewInitVault(vaultType string) (vaultmodel.Vault, error) {
+// 	if vaultType == "" {
+// 		return nil, fmt.Errorf("vault type cannot be empty")
+// 	}
 
-	WizardWarmup(map[string]string) error
-	WizardNext() *huh.Form
-	WizardComplete() map[string]string
-}
+// 	vault, ok := Registry[vaultType]
+// 	if !ok {
+// 		return nil, fmt.Errorf("unknown vault type: %s", vaultType)
+// 	}
+// 	return vault, nil
+// }
 
-type VaultOptions struct {
-	ReplaceHyphen     bool     `toml:"replaceHyphen"`
-	AutoUppercase     bool     `toml:"autoUppercase"`
-	IgnoreContentType []string `toml:"ignoreContentType"`
-}
+// func NewVault(vaultType string, options map[string]string) (vaultmodel.Vault, error) {
+// 	if vaultType == "" {
+// 		return nil, fmt.Errorf("vault type cannot be empty")
+// 	}
 
-func VaildateVaultOpts(opts map[string]string) error {
-	if opts["VAULT_TYPE"] == "" {
-		return fmt.Errorf("vault type cannot be empty")
-	}
+// 	v, err := NewInitVault(vaultType)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	v.SetOptions(options)
+// 	err = v.Warmup()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return v, nil
+// }
 
-	vlt, ok := Registry[opts["VAULT_TYPE"]]
-	if !ok {
-		return fmt.Errorf("unknown vault type: %s", opts["VAULT_TYPE"])
-	}
+// //go:embed template
+// var template string
 
-	vlt.ValidateConfig(opts)
+// // write vault file
+// func WriteFile(path string, options map[string]string) error {
+// 	path = GetVaultPath(path)
 
-	return nil
-}
+// 	if options["VAULT_TYPE"] == "" {
+// 		slog.Debug("please, developer, add 'VAULT_TYPE' as output to GetOptions()")
+// 		return fmt.Errorf("vault type cannot be empty")
+// 	}
 
-// open vault from path
-func OpenVault(path string) (Vault, error) {
-	opts, err := tools.GetVaultFile(path)
-	if err != nil {
-		return nil, err
-	}
+// 	out := make([]string, 0)
+// 	out = append(out, template)
+// 	s, err := godotenv.Marshal(options)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	out = append(out, s)
 
-	v, err := NewInitVault(opts["VAULT_TYPE"])
-	if err != nil {
-		return nil, err
-	}
+// 	//str to byte
+// 	out = append(out, "\n")
+// 	//0644:rw-r--r--
+// 	err = os.WriteFile(path, []byte(strings.Join(out, "\n")), 0644)
+// 	if err != nil {
+// 		panic("failed to write file: " + err.Error())
+// 	}
 
-	err = v.SetOptions(opts)
-	if err != nil {
-		return nil, err
-	}
-
-	return v, nil
-}
-
-func NewInitVault(vaultType string) (Vault, error) {
-	if vaultType == "" {
-		return nil, fmt.Errorf("vault type cannot be empty")
-	}
-
-	vault, ok := Registry[vaultType]
-	if !ok {
-		return nil, fmt.Errorf("unknown vault type: %s", vaultType)
-	}
-	return vault, nil
-}
-
-func NewVault(vaultType string, options map[string]string) (Vault, error) {
-	if vaultType == "" {
-		return nil, fmt.Errorf("vault type cannot be empty")
-	}
-
-	v, err := NewInitVault(vaultType)
-	if err != nil {
-		return nil, err
-	}
-	v.SetOptions(options)
-	err = v.Warmup()
-	if err != nil {
-		return nil, err
-	}
-	return v, nil
-}
-
-//go:embed template
-var template string
-
-// write vault file
-func WriteFile(path string, options map[string]string) error {
-	path = GetVaultPath(path)
-
-	if options["VAULT_TYPE"] == "" {
-		slog.Debug("please, developer, add 'VAULT_TYPE' as output to GetOptions()")
-		return fmt.Errorf("vault type cannot be empty")
-	}
-
-	out := make([]string, 0)
-	out = append(out, template)
-	s, err := godotenv.Marshal(options)
-	if err != nil {
-		return err
-	}
-	out = append(out, s)
-
-	//str to byte
-	out = append(out, "\n")
-	//0644:rw-r--r--
-	err = os.WriteFile(path, []byte(strings.Join(out, "\n")), 0644)
-	if err != nil {
-		panic("failed to write file: " + err.Error())
-	}
-
-	return nil
-}
+// 	return nil
+// }
