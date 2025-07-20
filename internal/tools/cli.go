59df4056a34bcc038cf35e21c11ae0abc571f812
Philip Meholm <mephim+git@gmail.com>
2025-07-20 18:03:46 +0200

    upd tools

--- internal/tools/cli.go
+++ internal/tools/cli.go
@@ -2,6 +2,8 @@ package tools
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"strings"
 )
 
@@ -18,12 +20,12 @@ func ExtractFilenameArg(args []string) (s string) {
 }
 
 // append .env extension to path if not already there
-func AppendDotEnvExtension(path string) string {
-	if strings.Contains(path, ".env") {
-		return path
-	}
-	return path + ".env"
-}
+// func AppendDotEnvExtension(path string) string {
+// 	if strings.Contains(path, ".env") {
+// 		return path
+// 	}
+// 	return ".env"path + ""
+// }
 
 // extract vault name from cobra args
 func ExtractVaultNameArg(args []string, vaults []string) (string, error) {
@@ -41,4 +43,21 @@ func ExtractVaultNameArg(args []string, vaults []string) (string, error) {
 		}
 	}
 	return "", nil
+}
+
+// gets path from either Path flag or positional argument
+func SetPathOrArg(Path string, args []string) string {
+	if len(args) >= 1 && Path != "" {
+		slog.Error("Both --path and positional arguments are set. Please use only one of the two.")
+		os.Exit(1)
+	} else if len(args) == 1 && Path == "" {
+		slog.Debug("using positional argument as path", "path", args[0])
+		Path = args[0]
+	}
+
+	if Path == "" {
+		slog.Error("no path set. please set --path or positional argument")
+		os.Exit(1)
+	}
+	return Path
 }
