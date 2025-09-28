ed288173fa448d6aaff3ec17d06ced803a950137
Philip Meholm <mephim+git@gmail.com>
2025-09-29 01:34:54 +0200

    upd licence on all files

--- internal/vaults/devvault/devvault.go
+++ internal/vaults/devvault/devvault.go
@@ -1,5 +1,8 @@
 //go:build !omitdevpackage
 
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // Package devvault contains a development vault that can be used for testing
 package devvault
 
